b89d55252f321d478f8051567edf369b8a71d4bd
dquiroga1 <136743226+dquiroga1@users.noreply.github.com>
2023-06-16 11:00:18 +0200

    QuirogaDiego_treball_ordinaria_part2.py.py

--- QuirogaDiego_treball_ordinaria_part2.py.py
+++ QuirogaDiego_treball_ordinaria_part2.py.py
@@ -1,6 +1,6 @@
 import sqlite3
 #obrir la biblioteca "sqlite3"
-diccionari = {}    
+diccionari = {}
 conexió = sqlite3.connect("Notes.db")
 #cream una connexió
 cursor = conexió.cursor()
@@ -9,15 +9,16 @@ taula = cursor.execute("SELECT * FROM Notes")
 #seleccionam tota la taula
 llista = taula.fetchall()
 #assignam la taula a una variable
-print(len(llista))
+print (llista[0][3:])
 estudiant = 0
 qualificacions = 3
 #cream variables auxiliars
-while estudiant < len(llista[0]):
+print(len(llista))
+while estudiant < len(llista):
     #iniciam el primer bucle
      suma = 0
      while qualificacions < len(llista[0][3:])+3:
-        #iniciam segon bucle
+        #iniciam segon bucle        
          suma += llista[estudiant][qualificacions]
          #actualitzam la nota
          qualificacions +=1
@@ -25,6 +26,7 @@ while estudiant < len(llista[0]):
     #calculam la mitjana
      qualificacions = 3
      estudiant +=1
+     
      print("Mitjana alumne",llista[estudiant-1][1],"és",suma,"\n")
      diccionari[llista[estudiant-1][1]] = suma
 print(diccionari)
